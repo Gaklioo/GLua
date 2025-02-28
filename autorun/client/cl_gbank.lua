ebf1aad19a166cd14a1a8566aa9c339446af20e8
sapatt <gaklio2321@gmail.com>
2025-02-27 21:48:05 -0500

    remove useless print
    remove useless print

--- autorun/client/cl_gbank.lua
+++ autorun/client/cl_gbank.lua
@@ -2,9 +2,7 @@ local curPlayerBal = 0
 
 net.Receive("gBankRecieveBalance", function()
     local bal = net.ReadUInt(32)
-    print("Bal: ", bal)
     curPlayerBal = bal
-    print("curBal: ", curPlayerBal )
 end)
 
 hook.Add("OnPlayerChat", "gBankCheckMoney", function(ply, strText, bTeam, bDead)
